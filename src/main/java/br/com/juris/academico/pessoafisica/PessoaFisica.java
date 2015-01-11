7bac984c313811b623d604466fa5f9e7f57c6324
icaromuniz <icaro.muniz@gmail.com>
2015-01-11 18:39:12 -0300

    Hibernate adicionado

---
+++ src/main/java/br/com/juris/academico/pessoafisica/PessoaFisica.java
@@ -0,0 +1,13 @@
+package br.com.juris.academico.pessoafisica;
+
+import br.com.juris.academico.geral.EntidadeAbs;
+
+public class PessoaFisica extends EntidadeAbs {
+
+	private static final long serialVersionUID = 6155130143446479872L;
+
+	public PessoaFisica() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

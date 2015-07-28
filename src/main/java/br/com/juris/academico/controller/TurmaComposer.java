a0142701a940d8256aa907ad560c1551f5f2158e
icaromuniz <icaro.muniz@gmail.com>
2015-07-28 19:32:04 -0300

    Desativação de alteração da Turma para Usuários não Administradores.

--- src/main/java/br/com/juris/academico/controller/TurmaComposer.java
+++ src/main/java/br/com/juris/academico/controller/TurmaComposer.java
@@ -1,15 +1,23 @@
 package br.com.juris.academico.controller;
 
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
 import org.zkoss.zul.Combobox;
+import org.zkoss.zul.Grid;
 import org.zkoss.zul.Textbox;
+import org.zkoss.zul.impl.InputElement;
 
 import br.com.juris.academico.arquitetura.AbstractComposer;
 import br.com.juris.academico.model.Turma;
+import br.com.juris.academico.model.Usuario;
 import br.com.juris.academico.persistence.TurmaDao;
 
 public class TurmaComposer extends AbstractComposer<Turma> {
 
 	private static final long serialVersionUID = -4900675433056035039L;
+	
+	// componentes do form
+	private Grid grid;
 
 	// componentes do list
 	private Textbox filtroNome;
@@ -17,6 +25,21 @@ public class TurmaComposer extends AbstractComposer<Turma> {
 	
 	public TurmaComposer() {
 		super(Turma.class);
+	}
+	
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		
+		super.doAfterCompose(comp);
+		
+		// desabilita edição da matrícula
+		if (Executions.getCurrent().getDesktop().getRequestPath().contains("/form.zul")) {
+			
+			Usuario usuario = Executions.getCurrent().getSession().getAttribute("usuario") != null ?
+					(Usuario) Executions.getCurrent().getSession().getAttribute("usuario") : null;
+			
+			atribuiPermissaoEdicao(grid, usuario != null && usuario.isAdministrador());
+		}
 	}
 
 	@Override
@@ -30,5 +53,18 @@ public class TurmaComposer extends AbstractComposer<Turma> {
 		setListaModelo(((TurmaDao)dao).findByFiltro(filtroNome.getValue(),
 				filtroUnidade.getSelectedItem() != null ? filtroUnidade.getSelectedItem().getLabel() : null));
 		getBinder().notifyChange(this, "*");
+	}
+	
+	private void atribuiPermissaoEdicao(Component component, boolean isEdicaoPermitida){
+		
+		if (component.getChildren() != null) {
+			for (Component c : component.getChildren()) {
+				atribuiPermissaoEdicao(c, isEdicaoPermitida);
+			}
+		}
+		
+		if(component instanceof InputElement && component.isVisible()){
+			((InputElement) component).setDisabled(!isEdicaoPermitida);
+		}
 	}
 }
